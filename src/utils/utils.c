5e882d5426de55810a1c2b2c9fa32858986effb1
pzl <pzl1891@qq.com>
2025-05-09 15:17:33 +0800

    feat(utils): 增加日志的微秒

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -5,6 +5,19 @@
 #include <stdarg.h>
 #include <string.h>
 #include <time.h>
+#include <sys/time.h>
+
+void get_time_with_ms(char *time_str, size_t len)
+{
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    struct tm *tm_info = localtime(&tv.tv_sec);
+    strftime(time_str, len, "%Y-%m-%d %H:%M:%S", tm_info);
+
+    // 将毫秒部分追加到字符串中
+    snprintf(time_str + strlen(time_str), len - strlen(time_str), ".%03d", (int)(tv.tv_usec / 1000));
+}
 
 void errif_debug(int line, const char *file, pthread_t pid, const char *fmt, ...)
 {
@@ -14,8 +27,8 @@ void errif_debug(int line, const char *file, pthread_t pid, const char *fmt, ...
     time_t now = time(NULL);
     struct tm tm_info;
     localtime_r(&now, &tm_info);
-    char time_str[20];
-    strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", &tm_info);
+    char time_str[25];
+    get_time_with_ms(time_str, sizeof(time_str)/sizeof(time_str[0]));
 
     const char *filename = strrchr(file, '/');
     if (filename == NULL) filename = file;
